f0d30e02d0acf43b775d58103e62cf916727c411
RolandoCornejo <54423436+RolandoCornejo@users.noreply.github.com>
2020-10-01 19:30:33 -0600

    Listas añadidas
    Ya le añadí una media lista de clientes de ejemplo y un medio proceso  para imprimir la info.
    Solo faltaría añadir el proceso de ingreso a las listas nada más

--- parcial2SIO/PARCIAL.c
+++ parcial2SIO/PARCIAL.c
@@ -3,24 +3,49 @@
 #include<unistd.h>
 #include<pthread.h>
 
-int a = 0;
+//Definiendo el estructura cliente
+typedef struct cliente {
+    char nombre[256];
+    int tiempo;
+    int prioridad;
+}cliente;
+
+typedef struct lista {
+            cliente actual;
+            cliente *caja1;
+            cliente *caja2;
+   	    }lista;
+
+
 
 void *func(void *var)
 {
-    int *id=(int *)var;
-    static int b=0;
-    b=7;
-    printf("the id of the thread is: %d\n", *id);
-    printf("the value if a is: %d\n", a);
-    printf("the value of b is: %d\n", b);
+    lista *id=(lista *)var;
+    
+    printf("Cliente: %s\n", id->actual.nombre);
+    printf("Tiempo: %d minutos\n", id->actual.tiempo);
+    printf("Prioridad: %d\n", id->actual.prioridad);
 }
 
 int main()
 {
     pthread_t th_id;
-    for(int i=0; i<9; i++)
+    cliente lista_clientes[]={
+            {.nombre="Maria",.tiempo=3,.prioridad=1},
+            {.nombre="Juan",.tiempo=10,.prioridad=2},
+            {.nombre="Josefa",.tiempo=3,.prioridad=4},
+        };
+        cliente caja1[]={0};
+        cliente caja2[]={0}; 
+
+    
+    
+    size_t n = sizeof(lista_clientes) / sizeof(lista_clientes[0]);
+    for(int i=0; i<n; i++)
     {
-        pthread_create(&th_id, NULL, func, (void*)&th_id);
+        lista datos={lista_clientes[i],caja1,caja2};
+ 
+        pthread_create(&th_id, NULL, func, (void*)&datos);
         pthread_join(th_id,NULL);
     }
     pthread_exit(NULL);
